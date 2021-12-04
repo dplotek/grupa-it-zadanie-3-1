27f1d97ff7c2fc46679f7d4c28cc75738a4e149c
Dawid Płotek <da.plotek@gmail.com>
2021-12-04 17:12:02 +0100

    upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# Instrukcja instalacji i konfiguracji
+
+Projekt stworzony dzięki [Create React App](https://github.com/facebook/create-react-app).
+
+## Aby uruchomić:
+
+Sklonuj repozytorium. Będziesz potrzebował `node` oraz `npm` zainstalowanych globalnie.
+
+### Instalacja
+
+`npm install`
+
+### Uruchomienie serwera
+
+`npm start`
+
+### Uruchomienie aplikacji
+
+Otwórz przeglądarkę i wprowadź adres `localhost:3000`
+
+### [Wersja live](dplotek.github.io/grupa-it-zadanie-3-1/)
---
+++ src/App.js
@@ -0,0 +1,74 @@
+import './App.css';
+import {useEffect,useState} from 'react'
+import QuoteBox from './components/QuoteBox';
+
+const App = () => {
+  const API = process.env.REACT_APP_API_KEY
+
+  const [allQuotes, setAllQuotes] = useState([])
+  const [quotes, setQuotes] = useState({
+    quoteToDisplay: '',
+    prevQuote: 'Najpierw wylosuj',
+    randomQuote:''
+  })
+
+  useEffect(() => {
+    const getQuotes = async () => {
+      await fetch(API)
+        .then(response => response.json())
+        .then(data => data.reduce((a,b)=> a.concat(b), []))
+        .then(data=> setAllQuotes(data))
+    }
+    getQuotes()
+  },[])
+
+  useEffect(() => {
+    if(allQuotes.length !== 0){
+      setRandomQuote()
+    }
+  }, [allQuotes])
+
+  const setRandomQuote = () => {
+    const numberOfQuotes = allQuotes.length
+    const drawnQuote = Math.floor(Math.random() * (numberOfQuotes - 1)) + 1;
+    setQuotes({
+      randomQuote: allQuotes[drawnQuote],
+      prevQuote: quotes.randomQuote,
+      quoteToDisplay: allQuotes[drawnQuote]
+    })
+  }
+
+  const displayPrevQuote = ()=> {
+    if(quotes.prevQuote === ""){
+      setQuotes(prevState => ({
+        ...prevState,
+        quoteToDisplay: '',
+      }))
+    }else {
+      setQuotes(prevState => ({
+        quoteToDisplay: quotes.prevQuote,
+        prevQuote: prevState.randomQuote,
+        randomQuote: quotes.prevQuote
+      }))
+    }
+  }
+
+  return (
+    <div className="wrapper">
+      <header>
+        <h1>Zadanie nr. 3.1</h1>
+        <p>Aplikacja, która pobiera cytaty z API. Po załadowaniu strony wyświetla losowy cytat, dodatkowo zostały dodane dwa przyciski: pierwszy losuje następny cytat, drugi pozwala na powrót do poprzedniego <strong>WYŚWIETLANEGO</strong> cytatu.</p>
+        <br/>
+        <strong>UWAGA. Druga wersja aplikacji, która wyświetla poprzedni WYLOSOWANY cytat dostępna <a href='https://dplotek.github.io/grupa-it-zadanie-3-2/' target="_blank">tutaj.</a></strong>
+      </header>
+      <div className="button__container">
+        <button onClick={setRandomQuote}>Losowy cytat</button>
+        <button onClick={displayPrevQuote}>Poprzedni cytat</button>
+      </div>
+
+      <QuoteBox author={quotes.quoteToDisplay.author}  quote={quotes.quoteToDisplay.quote }/>
+    </div>
+  );
+}
+
+export default App;
